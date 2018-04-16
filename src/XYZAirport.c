62e68b700e42ada9f158654760122f39c2c4293e
akGMIT <G00328529@gmit.ie>
2018-04-16 13:07:21 +0100

    Functions implemented: int validateEmail(char email[]); int * statisticsMenu(int statisticsOptions[]); int statisticsSecondMenu(); void generateStatisticsProcess(Passenger * head, int statOptions[]); float generateStatsByTravelClassAndTravelOrigin(Passenger ** head, int travelClass, int option);

--- src/XYZAirport.c
+++ src/XYZAirport.c
@@ -10,8 +10,26 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+
+#define UK 1
+#define RESTOFEUROPE 2
+#define ASIA 3
+#define AMERICAS 4
+#define AUSTRALASIA 5
+#define ECONOMY 1
+#define PREMIUM 2
+#define BUSSINES 3
+#define FIRST 4
+#define THREE_TIMES 1
+#define FIVE_TIMES 2
+#define MORE_THAN_FIVE 3
+#define ONE_DAY 1
+#define THREE_DAYS 2
+#define SEVEN_DAYS 3
+#define MORE_THAN_SEVEN 4
 
 typedef struct Passenger{
     int passportNumber;
@@ -26,6 +44,7 @@ typedef struct Passenger{
     struct Passenger * next;
 }Passenger;
 
+int login();
 void addPassengerAtStart(struct Passenger** head);
 struct Passenger * createPassenger(Passenger * head, int passportNum);
 void displayList(struct Passenger* head);
@@ -39,11 +58,24 @@ void displayPassenger(Passenger ** head, int update);
 void printPassengerDetails(Passenger * head, int position);
 void deletePassenger(Passenger ** head);
 int listLength(Passenger * head);
+int validateEmail(char email[]);
+int * statisticsMenu(int statisticsOptions[]);
+int statisticsSecondMenu();
+void generateStatisticsProcess(Passenger * head, int statOptions[]);
+float generateStatsByTravelClassAndTravelOrigin(Passenger ** head, int travelClass, int option);
 
 void main() {
 
     struct Passenger* headPtr = NULL;
     int choice;
+    int loggedIn = 0;
+    int statisticsOptions[3];
+
+    do
+    {
+    	loggedIn = login();
+    }
+    while(loggedIn == 0);
 
     loadPassengerListFile(&headPtr);
     sortList(&headPtr);
@@ -53,8 +85,8 @@ void main() {
     printf("3. Display a passengers details.\n");
     printf("4. Update a passenger.\n");
     printf("5. Delete a passenger.\n");
-    printf("6. Generate statistics.");
-    printf("7. Print all passengers details to report file.");
+    printf("6. Generate statistics.\n");
+    printf("7. Print all passengers details to report file.\n");
 
     scanf("%d", &choice);
 
@@ -77,17 +109,221 @@ void main() {
         case 5:
         	deletePassenger(&headPtr);
         	break;
+        case 6:
+        	statisticsMenu(statisticsOptions);
+        	generateStatisticsProcess(headPtr, statisticsOptions);
+        	//printf("%d %d %d\n", statisticsOptions[0], statisticsOptions[1], statisticsOptions[2]);
+        	break;
         }
         printf("1. Add new passenger.\n");
         printf("2. Display all passengers to screen.\n");
         printf("3. Display a passengers details.\n");
         printf("4. Update a passenger.\n");
         printf("5. Delete a passenger.\n");
-        printf("6. Generate statistics.");
-        printf("7. Print all passengers details to report file.");
+        printf("6. Generate statistics.\n");
+        printf("7. Print all passengers details to report file.\n");
 
         scanf("%d", &choice);
     }
+}
+
+float generateStatsByTravelClassAndTravelOrigin(Passenger ** head, int travelClass, int option)
+{
+	Passenger * curr = *head;
+	int count = 0;
+	float percentage;
+	int len = listLength(*head);
+
+	while(curr != NULL)
+	{
+		if(curr->travelClass == travelClass && curr->travelOrigin == option)
+		{
+			count++;
+			printf("%d\n", count);
+		}
+		curr = curr->next;
+	}
+	printf("Count: %d\n", count);
+	printf("LEngth %d\n", listLength(*head));
+	percentage = (count / len) * 100;
+	return percentage;
+}
+
+void generateStatisticsProcess(Passenger * head, int statOptions[])
+{
+	//Passenger * curr;
+	//curr = head;
+
+
+	switch(statOptions[1])
+	{
+	case ECONOMY:
+	case PREMIUM:
+	case BUSSINES:
+	case FIRST:
+		if(statOptions[2] <= 5)
+			printf("---- %f ---", generateStatsByTravelClassAndTravelOrigin(&head, statOptions[1], statOptions[2]));
+		//if(statOptions[2] >= 6)
+			//generateStatsByTravelClassAndDurationOfStay(head, statOptions[1], statOptions[2]);
+		break;
+	case 0:
+		if(statOptions[2] <= 5)
+			//generateStatsByYearBornAndTravelOrigin(head, statOptions[1], statOptions[2]);
+		if(statOptions[2] >= 6)
+			//generateStatsByYearBornAndDurationOfStay(head, statOptions[1], statOptions[2]);
+		break;
+	}
+}
+
+int * statisticsMenu(int options[])
+{
+	int choice;
+	int correctInput = 1;
+
+	do
+	{
+		correctInput = 1;
+		choice = 0;
+
+		printf("Choose criteria for statistics: \n");
+		printf("1. Travel class. \n2. Born before 1980.\n");
+
+		scanf("%d", &choice);
+
+		if(choice == 1)
+		{
+			options[0] = 1;
+			printf("Choose travel class: \n");
+			printf("1. Economy.\n2. Premium economy.\n3. Business.\n 4. First.\n");
+
+			scanf("%d", &choice);
+
+			if(choice == 1)
+			{
+				options[1] = ECONOMY;
+			}
+			else if(choice == 2)
+			{
+				options[1] = PREMIUM;
+			}
+			else if(choice == 3)
+			{
+				options[1] = BUSSINES;
+			}
+			else if(choice == 4)
+			{
+				options[1] = FIRST;
+			}
+			else
+			{
+				printf("Incorrect input, try again.\n");
+				correctInput = 0;
+				continue;
+			}
+			options[2] = statisticsSecondMenu();
+		}
+		else if(choice == 2)
+		{
+			options[0] = 2;
+			options[1] = 0;
+			options[2] = statisticsSecondMenu();
+		}
+		else
+		{
+			printf("Incorrect input.\n");
+			correctInput = 0;
+			continue;
+		}
+	}
+	while(correctInput != 1);
+
+	return options;
+}
+
+int statisticsSecondMenu()
+{
+	int choice;
+
+	printf("Choose one of the available statistics: \n");
+	printf("1. %% of passengers who travel from the UK\n");
+	printf("2. %% of passengers who travel from the Rest of Europe");
+	printf("3. %% of players who travel from the Asia\n");
+	printf("4. %% of passengers who travel from the Americas\n");
+	printf("5. %% of passengers who travel from the Australasia\n");
+	printf("6. %% of passengers who spent on average one day in Ireland\n");
+	printf("7. %% of passengers who spent on average less than 3 days in Ireland\n");
+	printf("8. %% of passengers who spent on average less than 7 days in Ireland\n");
+	printf("9. %% of passengers who spent on average more than 7 days in Ireland\n");
+
+	scanf("%d", &choice);
+
+	return choice;
+}
+
+int validateEmail(char email[])
+{
+	const char s[2] = "@.";
+	const char com[3] = "com";
+	char *tok[3];
+	char *token;
+	int tokenCount;
+
+	token = strtok(email, s);
+	tokenCount = 0;
+
+	while( token != NULL)
+	{
+		tok[tokenCount] = token;
+
+		token = strtok(NULL, s);
+
+		if(token != NULL)tokenCount++;
+	}
+
+	if(tokenCount != 2 || strcmp(tok[2], com) != 0)
+	{
+		printf("Email format is incorrect, try again.\n");
+		return 0;
+	}
+	return 1;
+}
+
+int login()
+{
+	char loginName[10];
+	char password[6];
+
+	FILE * fptr;
+	fptr = fopen("auth.dat", "r");
+
+	if(fptr == NULL)
+	{
+		printf("Authentification file could not be opened;");
+		exit(0);
+	}
+	else
+	{
+		printf("Login: ");
+		scanf("%s", loginName);
+		printf("Password: ");
+		scanf("%s", password);
+
+		while(!feof(fptr))
+		{
+			char loginFromFile[10];
+			char passwordFromFile[6];
+
+			fscanf(fptr, "%s %s", loginFromFile, passwordFromFile);
+
+			if(strcmp(loginName, loginFromFile) == 0 && strcmp(password, passwordFromFile) == 0)
+			{
+				printf("Login successful.\n\n");
+				return 1;
+			}
+		}
+		printf("Login or password incorrect. Try again.\n\n");
+		return 0;
+	}
 }
 
 void loadPassengerListFile(Passenger ** head)
@@ -176,11 +412,15 @@ struct Passenger * createPassenger(Passenger * head, int passportNum)
         scanf("%s", newPassenger->firstName);
         printf("Enter second name: ");
         scanf("%s", newPassenger->secondName);
-        /*printf("Enter date of birth in format ddmmyyyy: ");
-        scanf("%d", &new_passenger->DOB);
-        printf("Enter passengers email: ");
-        scanf("%s", new_passenger->email);
-        printf("Enter passengers origin of travel: ");
+        //printf("Enter date of birth in format ddmmyyyy: ");
+        //scanf("%d", &newPassenger->DOB);
+        do
+        {
+        	printf("Enter passengers email: ");
+        	scanf("%s", newPassenger->email);
+        }
+        while(validateEmail(newPassenger->email) != 1);
+        /*printf("Enter passengers origin of travel: ");
         printf("1. UK\n2. Rest of Europe \n3. Asia \n4. Americas \n5. Australasia");
         scanf("%d", &new_passenger->travelOrigin);
         printf("Enter passengers travel class: ");
@@ -424,7 +664,6 @@ void deletePassenger(Passenger ** head)
 		prev_curr = curr;
 		curr = curr->next;
 	}
-
 	prev_curr->next = curr->next;
 	free(curr);
 
@@ -440,7 +679,6 @@ int listLength(Passenger * head)
 		len++;
 		curr = curr->next;
 	}
-
 	return len;
 }
 
